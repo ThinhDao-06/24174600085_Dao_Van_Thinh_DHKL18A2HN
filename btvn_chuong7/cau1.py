83edb6431cfd25253d9a31efbc89d113f0607d0e
Thịnh Đào <dvthinh.24174600085@sv.uneti.edu.vn>
2024-10-23 22:07:57 +0700

    btvn

--- btvn_chuong7/cau1.py
+++ btvn_chuong7/cau1.py
@@ -1,22 +1,26 @@
 import math
 
-# Khai báo giá trị pi
-pi = 3.14
-
 # Nhập bán kính và chiều cao từ bàn phím
 r = float(input("Nhập bán kính của khối trụ: "))
 h = float(input("Nhập chiều cao của khối trụ: "))
 
-# Tính diện tích xung quanh
-dien_tich_xung_quanh = 2 * pi * r * h
+# Điều kiện
+if r > 0 and h > 0:
+    pi = 3.14
+    # Tính diện tích xung quanh
+    dien_tich_xung_quanh = 2 * pi * r * h
 
-# Tính diện tích toàn phần
-dien_tich_toan_phan = 2 * pi * r * (r + h)
+    # Tính diện tích toàn phần
+    dien_tich_toan_phan = 2 * pi * r * (r + h)
 
-# Tính thể tích
-the_tich = pi * r**2 * h
+    # Tính thể tích
+    the_tich = pi * r**2 * h
 
-# In kết quả và làm tròn đến chữ số thập phân thứ 2
-print("Diện tích xung quanh của khối trụ là: {:.2f}".format(dien_tich_xung_quanh))
-print("Diện tích toàn phần của khối trụ là: {:.2f}".format(dien_tich_toan_phan))
-print("Thể tích của khối trụ là: {:.2f}".format(the_tich))
+    # In kết quả và làm tròn đến chữ số thập phân thứ 2
+    print("Diện tích xung quanh của khối trụ là: {:.2f}".format(dien_tich_xung_quanh))
+    print("Diện tích toàn phần của khối trụ là: {:.2f}".format(dien_tich_toan_phan))
+    print("Thể tích của khối trụ là: {:.2f}".format(the_tich))
+else:
+    print("r va h phai lon hon 0")
+
+
