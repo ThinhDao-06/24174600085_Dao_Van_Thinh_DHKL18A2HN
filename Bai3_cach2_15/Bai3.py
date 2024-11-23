09248186738a8455501e363faf884c4c59643283
Thịnh Đào Văn <dvthinh.24174600085@sv.uneti.edu.vn>
2024-11-23 20:36:52 +0700

    Delete Bai3_cach2_15 directory

--- Bai3_cach2_15/Bai3.py
+++
@@ -1,6 +0,0 @@
-ho_va_ten = input("Nhap vao ten:")
-ten_nhap_vao = ho_va_ten.split()
-ho_ten=ten_nhap_vao[0]             #Họ chạy từ 0
-ten_dem=" ".join(ten_nhap_vao[1:]) #Tên đệm chạy từ 1 đến còn lại
-print(f"Ho: {ho_ten}")
-print(f"Ten dem: {ten_dem}")
